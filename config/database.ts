8550504cafe1a2acf6fceff435d97d958b365045
Pimenta <mateuspimartins@hotmail.com>
2022-07-10 01:54:52 -0300

    first commit

---
+++ config/database.ts
@@ -0,0 +1,19 @@
+import pg from 'pg';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pg;
+
+const connectionsConfig: any = new Pool({
+  connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/postgres',
+});
+
+if (process.env.MODE === 'PROD') {
+  connectionsConfig.ssl = {
+    rejectUnauthorized: false,
+  }
+}
+
+const db = connectionsConfig;
+export default db;
---
+++ index.ts
@@ -0,0 +1,19 @@
+import express from "express"
+import "express-async-errors"
+import cors from "cors"
+import dotenv from "dotenv"
+import { Routers } from "./routers/index.js"
+
+dotenv.config()
+
+const App = express()
+App.use(express.json())
+App.use(cors())
+
+App.use(Routers)
+
+const port = +process.env.PORT || 4000
+
+App.listen(port, () => {
+  console.log(`Server online and listening on port ${port}`)
+})
